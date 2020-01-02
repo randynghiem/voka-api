a24ae175af74fe4cb8536de499cb3132d67f85f2
Randy Nghiem <randy.nghiem@hotmail.com>
2020-01-02 12:48:15 +0800

    prepare for initial deployment

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+
 const apiRoutes = require('./routes/api');
 const handleNotFound = require('./routes/middleware/not-found');
 const handleCatchErrors = require('./routes/middleware/catch-errors');
@@ -6,6 +9,12 @@ const handleCatchErrors = require('./routes/middleware/catch-errors');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+/**
+ * Optimize for deployment
+ */
+app.use(compression());
+app.use(helmet());
 
 /**
  * Configure Routing and middlewares
