8bb4d6fcf3ce610bd71dfea49a1d6b98e5aa3159
Randy Nghiem <randy.nghiem@hotmail.com>
2020-01-19 19:35:54 +0800

    update sets router to support filtering by id

--- routes/api/sets-router.js
+++ routes/api/sets-router.js
@@ -4,11 +4,11 @@ const debug = require('debug')('voka-api:sets');
 const express = require('express');
 const router = express.Router();
 
-router.get('/:level', async (req, res, next) => {
+router.get('/:level', async (req, res) => {
 
   const { level } = req.params;
   //current support level A1 only
-  if (level.toLowerCase() !== 'a1') {
+  if (level !== 'a1') {
     res.status(404).json({ message: "Invalid level!" });
   }
 
@@ -16,6 +16,28 @@ router.get('/:level', async (req, res, next) => {
   fs.readFile(fullPath, { encoding: 'utf-8' }, (err, data) => {
     if (!err) {
       res.status(200).json(JSON.parse(data));
+    } else {
+      debug(err);
+    }
+  });
+});
+
+router.get('/:level/:id', async (req, res) => {
+  const { level, id } = req.params;
+  if (level !== "a1" || !id) {
+    res.status(404).json({ message: "Entry not found!" });
+  }
+
+  const fullPath = path.join(__dirname, '../../dataset/Thema.json');
+  fs.readFile(fullPath, { encoding: 'utf-8' }, (err, data) => {
+    if (!err) {
+      const items = JSON.parse(data);
+      const results = items.filter(it => it.id == id);
+      if (results.length === 1) {
+        res.json(results[0]);
+      } else {
+        res.status(404).json({});
+      }
     } else {
       debug(err);
     }
